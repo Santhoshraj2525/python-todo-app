adc1b561692175cef7773fc7011c6d7c006b3e16
santhoshraj <santhoshraj2525@gmail.com>
2026-04-08 22:05:46 -0600

    Initial commit for todo app

---
+++ modules/functions.py
@@ -0,0 +1,13 @@
+FILEPATH = "files/subfiles/todos.txt"
+
+def get_todos(filepath=FILEPATH):
+    """ Return the list of To-dos from the todos.txt file."""
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg,filepath=FILEPATH):
+    """ Write the to-do items list in the text file."""
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_arg)
